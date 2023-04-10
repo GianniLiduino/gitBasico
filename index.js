8e75be63fabde07905ff6109ea23c6cbae5b9b29
Gianni Liduino Ciuldin <gianniliduinociuldin@outlook.com>
2023-04-10 17:53:06 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const mysql = require('mysql');
+
+const app = express()
+
+const port = 8080;
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nodejs'
+});
+
+connection.connect(console.log('Conexão realizada com sucesso!'))
+
+
+app.get('/', (req, res) => {
+    connection.query(
+        'SELECT * FROM clients WHERE status = ? ORDER BY id DESC',
+        [1],
+        (err, results) => {
+            res.send(results);
+        }
+    );
+})
+
+app.get('/contato', (req, res) => {
+    res.send('Página contato!');
+})
+
+app.listen(port, console.log(`Servidor rodando na porta: ${port}`))
